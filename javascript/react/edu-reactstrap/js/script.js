598e80e467cc8afa9a66dd10111733b2d9db1984
tomual <tomual@users.noreply.github.com>
2017-11-21 22:54:07 -0600

    ReactStrap

---
+++ javascript/react/edu-reactstrap/js/script.js
@@ -0,0 +1,16 @@
+class Theme extends React.Component{
+    render(){
+        return(
+            <div>
+            <h1>THEME COMPONENT</h1>
+            </div>
+        );
+    }
+};
+
+ReactDOM.render(
+    <div>
+        Theme />
+    </div>,
+    document.getElementById('root')
+);

68526a34a6eff825a3268c87db5a7be28ed12676
tomual <tomual@users.noreply.github.com>
2018-02-07 13:16:42 -0600

    Horizontal, Vertical, Form Layouts

--- java/vaadin/src/main/java/com/vaadintest/MyUI.java
+++ java/vaadin/src/main/java/com/vaadintest/MyUI.java
@@ -4,13 +4,10 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.*;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -24,20 +21,76 @@ public class MyUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
+        initForm();
+    }
 
-        Button button = new Button("Click Me");
-        button.addClickListener(e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
+    private void initForm() {
+        VerticalLayout verticalLayout = new VerticalLayout();
+
+        FormLayout formLayout = new FormLayout();
+
+        TextField nameField = new TextField("Name");
+        nameField.setIcon(VaadinIcons.USER);
+        nameField.setRequiredIndicatorVisible(true);
+
+        TextField emailField = new TextField("Email");
+        emailField.setIcon(VaadinIcons.ENVELOPE);
+        emailField.setRequiredIndicatorVisible(true);
+
+        formLayout.addComponents(nameField, emailField);
+        verticalLayout.addComponent(formLayout);
+        verticalLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+
+        setContent(verticalLayout);
+    }
+
+    private void initVertical() {
+        VerticalLayout verticalLayout = new VerticalLayout();
+
+        TextField nameField = new TextField();
+        nameField.setCaption("Name");
+
+        TextField usernameField = new TextField();
+        usernameField.setCaption("Username");
+
+        PasswordField passwordField = new PasswordField();
+        passwordField.setCaption("Password");
+
+        Label nameLabel = new Label();
+
+        Button login = new Button("Login!");
+        login.addClickListener(e-> {
+            nameLabel.setValue(usernameField.getValue());
+            Notification.show("Hello " + usernameField.getValue());
         });
-        
-        layout.addComponents(name, button);
-        
-        setContent(layout);
+
+        verticalLayout.setMargin(true);
+        verticalLayout.setSpacing(true);
+        verticalLayout.setDefaultComponentAlignment(Alignment.MIDDLE_CENTER);
+        verticalLayout.addComponents(nameField, usernameField, passwordField, login, nameLabel);
+
+        setContent(verticalLayout);
+    }
+
+    private void initHorizontal() {
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+
+        TextField usernameField = new TextField();
+        usernameField.setCaption("Username");
+
+        Label nameLabel = new Label();
+
+        Button login = new Button("Login!");
+        login.addClickListener(e-> {
+            nameLabel.setValue(usernameField.getValue());
+            Notification.show("Hello " + usernameField.getValue());
+        });
+
+        horizontalLayout.setMargin(true);
+        horizontalLayout.setSpacing(true);
+        horizontalLayout.addComponents(usernameField, login, nameLabel);
+
+        setContent(horizontalLayout);
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)

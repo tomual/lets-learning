e8a53a49df2c5453a264fd42b78fc9e963e8f9e0
tomual <tomual@users.noreply.github.com>
2018-02-17 10:23:07 -0600

    C# init

---
+++ cs/HelloWorld/HelloWorld.UI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Some Cheeseburgers");
+            Console.ReadLine();
+        }
+    }
+}

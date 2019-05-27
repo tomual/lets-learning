bbfc4b59451d695d8b2e7b3eeb0e83fd591cde51
tomual <tomual@users.noreply.github.com>
2019-05-27 14:42:41 +0800

    Game end screen/quit

---
+++ unity/3D Beginner/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    bool m_IsPlayerAtExit;
+    float m_Timer;
+    public CanvasGroup exitBackgoundImageCanvasGroup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_IsPlayerAtExit)
+        {
+            EndLevel();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    void EndLevel()
+    {
+        m_Timer += Time.deltaTime;
+        exitBackgoundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
+        }
+    }
+}

017637b4bfb01b032059814f9de445d0fedecacf
tomual <tomual@users.noreply.github.com>
2019-05-31 16:34:12 +0800

    Game audio - ambient/step/ghost

--- unity/3D Beginner/Assets/Scripts/GameEnding.cs
+++ unity/3D Beginner/Assets/Scripts/GameEnding.cs
@@ -13,6 +13,9 @@ public class GameEnding : MonoBehaviour
     float m_Timer;
     public CanvasGroup exitBackgoundImageCanvasGroup;
     public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public AudioSource exitAudio;
+    public AudioSource caughtAudio;
+    bool m_HasAudioPlayed;
 
 
     // Start is called before the first frame update
@@ -26,10 +29,10 @@ public class GameEnding : MonoBehaviour
     {
         if (m_IsPlayerAtExit)
         {
-            EndLevel(exitBackgoundImageCanvasGroup, false);
+            EndLevel(exitBackgoundImageCanvasGroup, false, exitAudio);
         } else if (m_IsPlayerCaught)
         {
-            EndLevel(caughtBackgroundImageCanvasGroup, true);
+            EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
         }
     }
 
@@ -46,7 +49,7 @@ public class GameEnding : MonoBehaviour
         m_IsPlayerCaught = true;
     }
 
-    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
     {
         m_Timer += Time.deltaTime;
         imageCanvasGroup.alpha = m_Timer / fadeDuration;
@@ -59,6 +62,11 @@ public class GameEnding : MonoBehaviour
             {
                 Application.Quit();
             }
+        }
+        if (!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
         }
     }
 }
--- unity/3D Beginner/Assets/Scripts/PlayerMovement.cs
+++ unity/3D Beginner/Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,14 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody m_Rigidbody;
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
+    AudioSource m_AudioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         m_Animator = GetComponent<Animator>();
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -31,6 +33,17 @@ public class PlayerMovement : MonoBehaviour
 
         bool isWalking = hasHorizontalInput || hasVerticalInput;
         m_Animator.SetBool("IsWalking", isWalking);
+
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+        } else
+        {
+            m_AudioSource.Stop();
+        }
 
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);

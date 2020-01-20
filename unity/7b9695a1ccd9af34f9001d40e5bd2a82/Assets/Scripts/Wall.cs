7a488b785456b432ebc230570c6e2ee924c52e62
tomual <tomual@users.noreply.github.com>
2020-01-20 21:52:14 +0800

    2D Roguelike Tutorial

---
+++ unity/7b9695a1ccd9af34f9001d40e5bd2a82/Assets/Scripts/Wall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public Sprite damageSprite;
+    public int hp = 4;
+
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void DamageWall(int loss)
+    {
+        spriteRenderer.sprite = damageSprite;
+        hp -= loss;
+        if (hp <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

00d541313daddfd2467117a6285ad7a68a731261
tomual <tomual@users.noreply.github.com>
2020-01-30 18:55:11 +0800

    2D parallax background

---
+++ unity/2d-experiments/Assets/Parallax/Scripts/Parallax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    float length;
+    float startPos;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    void Start()
+    {
+        startPos = transform.position.x;
+    }
+
+    void Update()
+    {
+        float distance = (cam.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startPos + distance, transform.position.y, transform.position.z);
+    }
+}
---
+++ unity/2d-experiments/Assets/Parallax/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        if (horizontal != 0)
+        {
+            Vector3 target = transform.position + new Vector3(horizontal, 0);
+            transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * 5f);
+        }
+    }
+}

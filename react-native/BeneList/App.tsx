c7b0a79652d7cca928a60879994487b7bd892088
tomual <tomual@users.noreply.github.com>
2020-03-03 16:48:43 +0800

    Use redux for list

--- react-native/BeneList/App.tsx
+++ react-native/BeneList/App.tsx
@@ -1,38 +1,44 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 
+import { actionCreators } from './todoListRedux'
 import List from './List'
 import Input from './Input'
 import Title from './Title'
+import store from './store'
 
 export default class App extends Component {
-    state = {
-        todos: ['Dark Souls', 'Dark Souls II', 'Dark Souls III']
+    state = {}
+
+    componentWillMount() {
+        const { todos } = store.getState()
+        this.setState({ todos })
+
+        this.unsubscribe = store.subscribe(() => {
+            const { todos } = store.getState()
+            this.setState({ todos })
+        })
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe()
     }
 
     onAddTodo = (text) => {
-        const { todos } = this.state
-
-        this.setState({
-            todos: [text, ...todos]
-        })
+        store.dispatch(actionCreators.add(text))
     }
 
     onRemoveTodo = (index) => {
-        const { todos } = this.state
-
-        this.setState({
-            todos: todos.filter((todo, i) => i !== index)
-        })
+        store.dispatch(actionCreators.remove(index))
     }
 
     render() {
         const { todos } = this.state
         return (
             <View>
-            <Title>To-Do List</Title>
-            <Input placeholder={'Type a new task'} onSubmitEditing={this.onAddTodo} />
-            <List list={todos} onPressItem={this.onRemoveTodo} />
+                <Title>To-Do List</Title>
+                <Input placeholder={'Type a new task'} onSubmitEditing={this.onAddTodo} />
+                <List list={todos} onPressItem={this.onRemoveTodo} />
             </View>
         )
     }

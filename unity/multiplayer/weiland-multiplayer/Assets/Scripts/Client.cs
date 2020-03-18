7fac330abc164ae6f4e5fcaee8b8eecca5e4b25f
tomual <tomual@users.noreply.github.com>
2020-03-18 20:57:26 +0800

    C# Networking Tutorial - Part 1
    https://www.youtube.com/watch?v=uh8XaC0Y5MA&list=PLXkn83W0QkfnqsK8I0RAz5AbUxfg3bOQ5&index=1

---
+++ unity/multiplayer/weiland-multiplayer/Assets/Scripts/Client.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    public static Client instance;
+    public static int dataBufferSize = 4096;
+
+    public string ip = "127.0.0.1";
+    public int port = 26950;
+    public int myId = 0;
+    public TCP tcp;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Instance already exists. Deleting.");
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        tcp = new TCP();
+    }
+
+    public void ConnectToServer()
+    {
+        Debug.Log("ConnectToServer");
+        tcp.Connect();
+    }
+
+    public class TCP
+    {
+        public TcpClient socket;
+
+        private NetworkStream stream;
+        private byte[] receiveBuffer;
+
+        public void Connect()
+        {
+            Debug.Log("Connect");
+            socket = new TcpClient
+            {
+                ReceiveBufferSize = dataBufferSize,
+                SendBufferSize = dataBufferSize
+            };
+
+            receiveBuffer = new byte[dataBufferSize];
+            socket.BeginConnect(instance.ip, instance.port, ConnectCallback, socket);
+        }
+
+        private void ConnectCallback(IAsyncResult _result)
+        {
+            Debug.Log("ConnectCallback");
+            socket.EndConnect(_result);
+
+            if (!socket.Connected)
+            {
+                Debug.Log("Not Connected");
+                return;
+            }
+
+            stream = socket.GetStream();
+
+            stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+        }
+
+        private void ReceiveCallback(IAsyncResult _result)
+        {
+            Debug.Log("ReceiveCallback");
+            try
+            {
+                int _byteLength = stream.EndRead(_result);
+                if (_byteLength <= 0)
+                {
+                    // TODO: dc
+                    return;
+                }
+
+                byte[] _data = new byte[_byteLength];
+                Array.Copy(receiveBuffer, _data, _byteLength);
+
+                // TODO: handle data
+                stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+            }
+            catch (Exception _ex)
+            {
+                Console.WriteLine($"Error receiving TCP data: {_ex}");
+                // TODO: dc
+            }
+        }
+    }
+}

d228d03ec204926fc325f8e202e1384d755be4a7
tomual <tomual@users.noreply.github.com>
2020-07-22 13:59:57 +0800

    Timer test

--- godot/intro/Hello.gd
+++ godot/intro/Hello.gd
@@ -1,16 +1,23 @@
 extends Panel
 
+var accum = 0
+var accum2 = 0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Button.connect("pressed", self, "_on_Button_pressed")
+	
+	
+func _on_Button_pressed():
+	$Label.text = "Hello!"
+	
+func _process(delta):
+	accum += 1
+	$Label.text = str(accum) # 'text' is a built-in label property.
+	
+func _physics_process(delta):
+	accum2 += 1
+	$Label2.text = str(accum2) # 'text' is a built-in label property.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Timer_timeout():
+	$Sprite.visible = !$Sprite.visible
